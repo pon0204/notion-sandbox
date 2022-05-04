1f1dc74611cc6cbbeff454e420f2d31831defbd6
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-05-04 23:01:22 +0900

    feat: 途中

--- notion-alfred/index.d.ts
+++ notion-alfred/index.d.ts
@@ -1,1 +1,1 @@
-declare const hoge: string;
+export {};
--- notion-alfred/index.ts
+++ notion-alfred/index.ts
@@ -1,3 +1,12 @@
-const hoge: string = 'hoge'
+import axios from 'axios'
 
-console.log(hoge)
+// fetch APIを作る
+
+// NotionのIdとTokenを指定する。
+
+// 引数にalfredのものを受け取る。
+
+const hoge = await axios.get(
+  'https://zipcloud.ibsnet.co.jp/api/search?zipcode=1500032'
+)
+console.log(hoge.data.results)
